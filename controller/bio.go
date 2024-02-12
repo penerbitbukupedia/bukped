b5e469657f526f5351b91691bca369ad92016331
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-12 17:24:22 +0700

    ok

--- controller/bio.go
+++ controller/bio.go
@@ -15,7 +15,7 @@ func UploadFotoProfil(ctx *fiber.Ctx) error {
 	// save to github
 	content, response, err := helper.GithubUpload(file)
 	if err != nil {
-		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"content": content, "response": response, "error": err.Error()})
 	}
 
 	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{"content": content, "response": response})
